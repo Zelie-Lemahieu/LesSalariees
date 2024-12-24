b7abb4677f3989ba3ed8de4132bfa1bd8a3de0f0
Zélie Lemahieu <zelielemahieu@gmail.com>
2024-12-24 14:22:59 +0100

    first commit

---
+++ ExerciceBibliothèque_24_12_24/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using ExerciceBibliothèque_24_12_24.classes;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialisation variables
+            int entreeUtilisateur = 0;
+            List<Livre> livres = new List<Livre>();
+            Livre nouveauLivre = new Livre("", "", 0, false);
+            //INSTANCIATION PAR LA CONSOLE D'OBJET:
+            do
+            {
+                Console.WriteLine("Voulez-vous entrer un nouveau livre ?\n1---oui\t\t0---non");
+                entreeUtilisateur = int.Parse(Console.ReadLine());
+                if (entreeUtilisateur == 1)
+                {
+                    Console.WriteLine("Entrer un titre, puis un auteur, puis une année de publiaction");
+                    nouveauLivre = new Livre(Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine()), false);
+                }
+            } while (entreeUtilisateur != 0);
+
+            //INSTANCIATION DES OBJETS "LIVRE":
+            Livre ZibiBooboop = new Livre("Zibi Booboop : Bogos Binted", "Glorp", -156241, false);
+            Livre lAgendaDuGeek = new Livre("L'Agenda Du Geek 2016-2017", "Squeezie", 2016, false);
+            Livre frigielEtFluffy = new Livre("Frigiel et Fluffy - Tome 1 - Le retour de l'Ender dragon", "Frigiel", 2016, false);
+
+            //Ajout des livres à la liste
+            livres.Add(ZibiBooboop);
+            livres.Add(lAgendaDuGeek);
+            livres.Add(frigielEtFluffy);
+            livres.Add(nouveauLivre);
+
+            //AFFICHAGE DÉTAILS DES LIVRES:
+            foreach(Livre livre in livres)
+            {
+                livre.AfficherDetails();
+            };
+            Console.WriteLine("\n\n");
+
+            //Simulation de gestion des livres:
+            ZibiBooboop.Rendre();
+            lAgendaDuGeek.Emprunter();
+            lAgendaDuGeek.Emprunter();
+            frigielEtFluffy.Rendre();
+            lAgendaDuGeek.Rendre();
+            ZibiBooboop.Emprunter();
+            frigielEtFluffy.Emprunter();
+
+
+        }
+    }
+}
---
+++ ExerciceBibliothèque_24_12_24/classes/Livre.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Ajout du dossier contenant les classes:
+using ExerciceBibliothèque_24_12_24.classes;
+
+namespace ExerciceBibliothèque_24_12_24.classes
+{
+    internal class Livre
+    {
+        //ATTRIBUTS
+        private String _titre;
+        private String _auteur;
+        private int _anneeDePublication;
+        private bool _estEmprunte;
+
+        //PROPRIÉTÉS
+        public String Titre { get { return _titre; } set { _titre = value; } }
+        public String Auteur { get { return _auteur; } set { _auteur = value; } }
+        public int AnneeDePublication { get { return _anneeDePublication; } set { _anneeDePublication = value; } }
+        public bool EstEmprunte { get { return _estEmprunte; } set { _estEmprunte = value; } }
+
+        //CONSTRUCTEUR
+        public Livre (string titre, string auteur, int anneeDePublication, bool estEmrunte)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            AnneeDePublication = anneeDePublication;
+            EstEmprunte = estEmrunte;
+        }
+
+        //CONSTRUCTEUR AUTEUR INCONNUE
+        public Livre (string titre, int anneeDePublication, bool estEmrunte)
+        {
+            Titre = titre;
+            Auteur = "Auteur Inconnu";
+            AnneeDePublication = anneeDePublication;
+            EstEmprunte = estEmrunte;
+        }
+
+
+        //MÉTHODE EMPRUNTER
+        public void Emprunter()
+        {
+            if (EstEmprunte == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\t---Impossible d'emprunter. Le livre \"{Titre}\" est déjà en cours d'emprunt.---");
+                Console.ResetColor();
+            } else
+            {
+                EstEmprunte = true;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Le livre \"{Titre}\" a bien été emprunté.");
+                Console.ResetColor();
+            }
+        }
+
+        //MÉTHODE RENDRE
+        public void Rendre()
+        {
+            if (EstEmprunte == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\t---Impossible de rendre. Le livre \"{Titre}\" n'est pas en cours d'emprunt.---");
+                Console.ResetColor();
+            }
+            else
+            {
+                EstEmprunte = false;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Le livre \"{Titre}\" a bien été rendu.");
+                Console.ResetColor();
+            }
+        }
+
+        //MÉTHODE AFFICHER DÉTAILS:
+        public void AfficherDetails()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"Titre : {Titre}, Auteur : {Auteur}, Année de publication : {AnneeDePublication}, Statut : ");
+            if (EstEmprunte == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Emprunté");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Disponible");
+            };
+        }
+    }
+}
